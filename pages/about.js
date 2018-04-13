454e0875a3db82d00fdbe896ef3367fd943cba50
inmaculada peinado <g.adita.na88@gmail.com>
2018-04-13 15:10:27 +0200

    add pagination links

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+const About = () => (
+    <div>
+      <p>About Page</p>
+            <div>
+            <Link href="/">
+            <button>Home Page</button>
+            </Link>
+        </div>
+    </div>
+  )
+  
+  export default About
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
+import Link from 'next/link'
+
 const Index = () => (
     <div>
       <p>Hello Next.js</p>
+      <div>
+            <Link href="/about">
+            <button>About Page</button>
+            </Link>
+        </div>
     </div>
   )
   
